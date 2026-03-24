a0e58ee79cbf9417c8a95de976167e2c15fa96e3
unknown <paulomalavasi44@gmail.com>
2026-03-23 22:14:26 -0300

    Entity

---
+++ clientes/src/main/java/com/ph/clientes/Entity/ClientesEntity.java
@@ -0,0 +1,52 @@
+package com.ph.clientes.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class ClientesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(name = "emailCliente")
+    private String email;
+    @Column(name = "telefoneCliente")
+    private String telefone;
+
+    public ClientesEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
